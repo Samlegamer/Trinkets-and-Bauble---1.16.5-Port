b7b66158dac16fc45faca3c92cbdc5ee836a6f53
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2021-10-12 22:50:33 +0200

    Add files via upload

---
+++ main/java/fr/samlegamer/xat/items/XATItemsRegistry.java
@@ -0,0 +1,23 @@
+package fr.samlegamer.xat.items;
+
+import fr.samlegamer.xat.Xat;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class XATItemsRegistry
+{
+	public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, Xat.MODID);
+	
+	//normal items
+	public static final RegistryObject<Item> GLOWING_INGOT = ITEMS_REGISTRY.register("glowing_ingot", () -> new Item(new Item.Properties().tab(Xat.XAT_ITEMS_TAB)));
+	public static final RegistryObject<Item> GLOWING_DUST = ITEMS_REGISTRY.register("glowing_dust", () -> new Item(new Item.Properties().tab(Xat.XAT_ITEMS_TAB)));
+	public static final RegistryObject<Item> GLOWING_GEM = ITEMS_REGISTRY.register("glowing_gem", () -> new Item(new Item.Properties().tab(Xat.XAT_ITEMS_TAB)));
+	public static final RegistryObject<Item> DRAGON_EYE = ITEMS_REGISTRY.register("dragon_eye", () -> new DragonEyeItem(new Item.Properties().stacksTo(1).tab(Xat.XAT_ITEMS_TAB).fireResistant()));
+	public static final RegistryObject<Item> GLOWING_RING = ITEMS_REGISTRY.register("glowing_ring", () -> new GlowingRingItem(new Item.Properties().stacksTo(1).tab(Xat.XAT_ITEMS_TAB)));
+	public static final RegistryObject<Item> DAMAGE_SHIELD = ITEMS_REGISTRY.register("damage_shield", () -> new DamageShieldItem(new Item.Properties().stacksTo(1).tab(Xat.XAT_ITEMS_TAB)));
+	public static final RegistryObject<Item> POISON_STONE = ITEMS_REGISTRY.register("poison_stone", () -> new PoisonStoneItem(new Item.Properties().stacksTo(1).tab(Xat.XAT_ITEMS_TAB)));
+	public static final RegistryObject<Item> WITHER_RING = ITEMS_REGISTRY.register("wither_ring", () -> new WitherRingItem(new Item.Properties().stacksTo(1).tab(Xat.XAT_ITEMS_TAB)));
+
+}
